96a1e36e152e0361ad086be52662369209ac18f3
josephsherif <josephsherif233@gmail.com>
2023-07-27 02:48:57 +0300

    first assignment

--- C-programming/lesson 3/Lab1/lab1.c
+++ C-programming/lesson 3/Lab1/lab1.c
@@ -4,13 +4,15 @@
  *  Created on: 25 Jul 2023
  *      Author: Joseph Sherif
  */
+/*
 #include <stdio.h>
 void main(){
 	int i,j;
 	for(i =0;i<10;i++){
 		for(j =i;j<10;j++){
-			printf("%d",j);
+	printf("%d",j);
 		}
 		printf("\n");
 	}
 }
+*/
